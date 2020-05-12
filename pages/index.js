83e57e20089ec0d185beb81ba4a04abf5ed9d542
Rainspistols <ffly@mail.ru>
2020-05-12 18:18:45 +0200

    pre-render and data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,32 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/post';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
+  const renderPosts = allPostsData.map(({ id, date, title }) => (
+    <li className={utilStyles.listItem} key={id}>
+      {title}
+      <br />
+      {id}
+      <br />
+      {date}
+    </li>
+  ));
+
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Witam!
-          <br />
-          <br /> Znam się na React, Netlify, Strapi, Next js, JAMstack,
-          JavaScript (ES6), HTML5, CSS3, Git, Webpack. Mam więcej niż 15
-          projektów na React. Mam doświadczenie w UI/UX. Znam Adobe XD,
-          Photoshop, illustrator, figma etc. Z przyjemnością podejmę się
-          wykonania zadania technicznego, jeśli takie istnieje.
-          <br />
-          <br />
-          <u>Dostępność - natychmiast</u>
-          <br />
-          <b>Portfolio</b> -
-          https://rainspistols.github.io/React-myPortfolio2020/; <br />
-          <b>Github</b> - https://github.com/Rainspistols;
-          <br />
-          <b>LinkedIn</b> - https://www.linkedin.com/in/galchenko-max/;
-          <br />
-          <b>Telefon</b> - 576 284 759;
-          <br /> Nie mogę pracować B2B bo jestem obcokrajowcem. UOP preferowana.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
-        </p>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>{renderPosts}</ul>
       </section>
     </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return { props: { allPostsData } };
+};
