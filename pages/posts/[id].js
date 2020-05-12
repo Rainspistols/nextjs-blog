75fe573fde16300fd9baa8c6862be72b2b5c09bb
Rainspistols <ffly@mail.ru>
2020-05-12 20:28:00 +0200

    change date to lowercase

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/post';
 import Head from 'next/head';
-import Date from '../../components/Date';
+import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css'
 
 const Post = ({ postData }) => {
